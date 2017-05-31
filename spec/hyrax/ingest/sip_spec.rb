dc422fb21a6a3bced61709fa0542f10091e8f99d
Andrew Myers <afredmyers@gmail.com>
2017-05-31 15:28:01 -0600

    Fixes tests for SIP class

--- spec/hyrax/ingest/sip_spec.rb
+++ spec/hyrax/ingest/sip_spec.rb
@@ -4,8 +4,20 @@ require 'hyrax/ingest/sip.rb'
 RSpec.describe Hyrax::Ingest::SIP do
   describe '.new' do
     context 'when given an invalid path' do
+      let(:path) { 'path/to/nothing' }
       it 'raises an InvalidSIPPath error' do
-        expect { described_class.new('path/to/nothing') }.to raise_error Hyrax::Ingest::Errors::InvalidSIPPath
+        expect { described_class.new(path: path) }.to raise_error Hyrax::Ingest::Errors::InvalidSIPPath
+      end
+    end
+  end
+
+  describe '#files' do
+    subject { described_class.new(path: "#{fixture_path}/sip_examples/fits_example_1.xml")}
+    context 'when given a path to a single file' do
+      it 'returns a list containing a single File object for the file specified by the path param' do
+        expect(subject.files.count).to eq 1
+        expect(subject.files.first).to be_a File
+        expect(File.basename(subject.files.first)).to eq 'fits_example_1.xml'
       end
     end
   end
